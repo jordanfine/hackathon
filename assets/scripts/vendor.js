d3e7a4c076136e7815cf5afce13f0b74a3d3f6df
Jordan Fine <jordan6fine@gmail.com>
2013-09-28 17:12:21 -0400

    loadash

--- assets/scripts/vendor.js
+++ assets/scripts/vendor.js
@@ -1,4 +1,5 @@
 //= vendor/jquery/1.10.2/jquery.js
+//= vendor/lodash/lodash.min.js
 //= vendor/angular/1.1.5/angular.js
 //= vendor/angular/1.1.5/angular-mocks.js
 //= vendor/angular/1.1.5/angular-resource.min.js
